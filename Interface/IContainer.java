660844723d765fd744f0055e1b47ab9ab461262d
Sergey <mendsbsv@gmail.com>
2023-12-24 11:56:14 +0300

    Add ATT02

---
+++ Interface/IContainer.java
@@ -0,0 +1,7 @@
+package Interface;
+
+public interface IContainer {
+    IBox addCustomToys(ICustomToy customToy);
+    IBox addFabricToys(IToysFabric fabricToys);
+    int size();
+}
---
+++ Interface/IToysFabric.java
@@ -0,0 +1,9 @@
+package Interface;
+
+import Model.Toy;
+
+public interface IToysFabric {
+    Toy getToy();
+    int getWeight();
+    public String getName();
+}
